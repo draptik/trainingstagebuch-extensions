4ce603fb5ae883ddecb56666bd069f73290ef19e
Patrick Drechsler <socialcoding@pdrechsler.de>
2012-03-09 00:37:40 +0100

    initial import

---
+++ application/ttb-extensions/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TtbExtensions::Application.config.secret_token = '8a5b40a3d5a511769d4c3b49ff1a698263bd2a4a044a4ed90f390274db918bae5802731efe8c0b8a0dd391f33b31881248a38e9a78acc92f5a04939c987898a3'
