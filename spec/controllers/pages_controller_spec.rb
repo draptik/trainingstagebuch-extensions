e91f8a546399e024b588940d62c078ffba8fc3be
Patrick Drechsler <socialcoding@pdrechsler.de>
2012-03-09 01:51:09 +0100

    layout/application

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -16,4 +18,16 @@ describe PagesController do
     end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Trainingstagebuch Extensions | About")
+    end
+  end
+  
 end
