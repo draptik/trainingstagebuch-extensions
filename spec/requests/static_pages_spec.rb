176d97ccaff4d0f3d44913a615293b1735d2fa0b
Patrick Drechsler <socialcoding@pdrechsler.de>
2012-03-10 19:29:55 +0100

    contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  # describe "GET /static_pages" do
-  #   it "works! (now write some real specs)" do
-  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-  #     get static_pages_index_path
-  #     response.status.should be(200)
-  #   end
-  # end
+
+  let(:base_title) { "Trainingstagebuch Extensions" }
 
   describe "Home page" do
 
@@ -15,11 +10,15 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_content('Trainingstagebuch Extensions')
     end
+
+    it "should have the h1 'Trainingstagebuch Extensions'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => "#{base_title}")
+    end
     
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Trainingstagebuch Extensions | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -30,10 +29,14 @@ describe "StaticPages" do
       page.should have_content('Help')
     end
 
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "Trainingstagebuch Extensions | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -44,10 +47,32 @@ describe "StaticPages" do
       page.should have_content('About Us')
     end
 
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-                        :text => "Trainingstagebuch Extensions | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end
 end
